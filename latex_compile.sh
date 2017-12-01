0d208f48e823abdeb94bcb805d2fbf268a12b33c
Miek Messerschmidt <mmesserschmidt@gmail.com>
2017-12-01 18:12:48 +0200

    better organization

--- latex_compile.sh
+++ latex_compile.sh
@@ -1,3 +1,3 @@
-nbmerge *.ipynb > "compiled/A Python Primer for Mathematics.ipynb"
+nbmerge chapters/*.ipynb > "compiled/A Python Primer for Mathematics.ipynb"
 jupyter nbconvert --to pdf --template report --output-dir compiled "compiled/A Python Primer for Mathematics.ipynb"
-pdftk cover/cover.pdf "compiled/A Python Primer for Mathematics.pdf" cat output compiled/result.pdf
+pdftk cover/cover.pdf cover/copyright.pdf "compiled/A Python Primer for Mathematics.pdf" cat output compiled/result.pdf
